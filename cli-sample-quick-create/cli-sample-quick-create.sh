d63ea6bd65010b71d33ce63996f31b6ed46d6aa3
neilpeterson <neilpeterson@outlook.com>
2017-02-03 11:10:46 -0800

    new script

---
+++ cli-sample-quick-create/cli-sample-quick-create.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+# Create a resource group.
+az group create --name myResourceGroup --location westeurope
+
+# Create a virtual machine. 
+az vm create \
+  --image UbuntuLTS \
+  --admin-username ops \
+  --ssh-key-value ~/.ssh/id_rsa.pub \
+  --resource-group myResourceGroup \
+  --location westus \
+  --name myVM
