23b84fb918dd85c70fcd0a64f1c534bfa29d6e00
neilpeterson <neilpeterson@outlook.com>
2017-02-06 19:30:35 -0800

    update

--- windows-virtual-machine/create-vm-quick/create-vm-quick.bat
+++ windows-virtual-machine/create-vm-quick/create-vm-quick.bat
@@ -1,18 +1,16 @@
-#!/bin/bash
+REM Variables
+set resourceGroupName=myResourceGroup
+set location=westeurope
 
-# Variables
-resourceGroupName=myResourceGroup
-location=westeurope
+REM Create a resource group.
+az group create --name %resourceGroupName% --location %location%
 
-# Create a resource group.
-az group create --name $resourceGroupName --location $location
-
-# Create a virtual machine. 
-az vm create \
-  --image UbuntuLTS \
-  --admin-username ops \
-  --ssh-key-value ~/.ssh/id_rsa.pub \
-  --resource-group $resourceGroupName \
-  --location $location \
-  --name myVM \
+REM Create a virtual machine. 
+az vm create ^
+  --image UbuntuLTS ^
+  --admin-username ops ^
+  --ssh-key-value ~/.ssh/id_rsa.pub ^
+  --resource-group %resourceGroupName% ^
+  --location %location% ^
+  --name myVM ^
   --no-wait
