eaeaa5a661ebc9ce7d720c455b9f35981764a87e
neilpeterson <neilpeterson@outlook.com>
2017-02-07 08:04:28 -0800

    updates

--- windows-virtual-machine/mount-os-disk/mount-os-disk.bat
+++ windows-virtual-machine/mount-os-disk/mount-os-disk.bat
@@ -1,24 +1,22 @@
-#!/bin/bash
+REM Variables
+set resourceGroupName=%myResourceGroup%
+set location=%westeurope%
+set sourcevm=%virtual-machine%
 
-# Variables
-resourceGroupName=myResourceGroup
-location=westeurope
-sourcevm=virtual-machine
+REM Get the URI for the source VM operating system disk.
+uri="$(az vm show -g %resourceGroupName% -n %sourcevm% --query [storageProfile.osDisk.vhd.uri] -o tsv)"
 
-# Get the URI for the source VM operating system disk.
-uri="$(az vm show -g $resourceGroupName -n $sourcevm --query [storageProfile.osDisk.vhd.uri] -o tsv)"
+REM Delete the source virtual machine, this will not delete the disk.
+az vm delete -g %resourceGroupName% -n %sourcevm%
 
-# Delete the source virtual machine, this will not delete the disk.
-az vm delete -g $resourceGroupName -n $sourcevm
-
-# Create a new virtual machine.
-az vm create \
-  --image UbuntuLTS \
-  --admin-username opsadmin \
-  --ssh-key-value ~/.ssh/id_rsa.pub \
-  --resource-group $resourceGroupName \
-  --location $location \
+REM Create a new virtual machine.
+az vm create ^
+  --image UbuntuLTS ^
+  --admin-username opsadmin ^
+  --ssh-key-value c:\ssh\id_rsa.pub ^
+  --resource-group %resourceGroupName% ^
+  --location %location% ^
   --name myVM
 
-# Attach the VHD as a data disk to the newly created VM
-az vm disk attach-existing --vm-name myVM -g $resourceGroupName --vhd $uri --lun 0 -n dd1
+REM Attach the VHD as a data disk to the newly created VM
+az vm disk attach-existing --vm-name myVM -g %resourceGroupName% --vhd %uri% --lun 0 -n dd1
